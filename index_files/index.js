69ad4e2707b2a5e07858335483dcb2459e590308
Rae Lovejoy <git@lvjy.xyz>
2024-09-26 02:45:17 -0700

    NVM

---
+++ index_files/index.js
@@ -0,0 +1,11 @@
+// Created by iWeb 3.0.1 local-build-20100620
+
+setTransparentGifURL('Media/transparent.gif');function applyEffects()
+{var registry=IWCreateEffectRegistry();registry.registerEffects({stroke_0:new IWEmptyStroke()});registry.applyEffects();}
+function hostedOnDM()
+{return false;}
+function onPageLoad()
+{loadMozillaCSS('index_files/indexMoz.css')
+detectBrowser();adjustLineHeightIfTooBig('id1');adjustFontSizeIfTooBig('id1');adjustLineHeightIfTooBig('id2');adjustFontSizeIfTooBig('id2');Widget.onload();fixupAllIEPNGBGs();fixAllIEPNGs('Media/transparent.gif');applyEffects()}
+function onPageUnload()
+{Widget.onunload();}
